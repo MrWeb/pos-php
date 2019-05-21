eb09c97b99ffe440f147c7ab9334fe13ad1996e4
Mr.Web <lombardi.nd@gmail.com>
2019-05-21 22:18:42 +0200

    WIP

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
 <?php
-require __DIR__ . '/vendor/mike42/escpos-php/autoload.php';
-use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
-use Mike42\Escpos\Printer;
-
-try {
-    $connector = new NetworkPrintConnector("172.20.10.2", 9100);
-
-    $printer = new Printer($connector);
-    $printer->text("Hello Mimllenials!!\n");
-    $printer->cut();
-
-    /* Close printer */
-    $printer->close();
-} catch (Exception $e) {
-    echo "Impossibile stampare, errore: " . $e->getMessage() . "\n";
-}
+header('location: https://www.cashout.credit/');
